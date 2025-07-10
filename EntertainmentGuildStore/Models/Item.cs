146a6c8e095012d4d5cee6404352c29155264054
Kamenrider-uki <shutao579@gmail.com>
2025-07-10 18:59:24 +0800

    item details + login / register modify
    Added product information page, modified login/registration page, modified login steps and functions

--- EntertainmentGuildStore/Models/Item.cs
+++ EntertainmentGuildStore/Models/Item.cs
@@ -6,5 +6,12 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string? ImageUrl { get; set; } = "/images/default1.jpg"; 
+        public string? SecondImageUrl { get; set; } = "/images/default2.jpg";
+        public string? Category { get; set; } = "Default";
+        public string? Feature1 { get; set; }
+        public string? Feature2 { get; set; }
+        public string? Feature3 { get; set; }
+        public string? Feature4 { get; set; }
     }
 }
